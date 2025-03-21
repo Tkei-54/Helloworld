b86314c59fcabe31a9cb409f892e1aef94b11bc0
Keith Tongi​ <keithtongi54@gmail.com>
2025-03-21 21:00:48 +0300

    Additional edits from my favourite film. For practice

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ def greet():
 
 if __name__ == "__main__":
     greet()
+print("Good morning my neighbours!")
+print("Hey, fuck you!")
+print("Yes, yes, fuck you too!")
+print("exerpt from Coming to America.")
